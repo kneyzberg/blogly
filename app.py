9a9f6b1beec84f9e756893844174d114e2687f02
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-03-10 12:07:14 -0800

    initial commit and install

---
+++ app.py
@@ -0,0 +1,15 @@
+"""Blogly application."""
+
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'our-secret-key'
+
+connect_db(app)
+db.create_all()
+
