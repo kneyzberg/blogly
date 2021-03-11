36f4cb196bb88a8ba56e55a00cbbc411ee502ad1
Jon K <Johnakao@gmail.com>
2021-03-11 12:36:52 -0800

    Setup to Test App Routes

--- tests_app.py
+++ tests_app.py
@@ -1,7 +1,28 @@
 from unittest import TestCase
 from app import app
+from flask import session
 
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 class TestBloglyApp(TestCase):
 
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
     
+    def test_index(self):
+        with self.client as client:
+            response = client.get('/')
+            html = response.get_data(as_text=True)
+            self.assertIn("<h1>", html)
+            self.assertEqual(response.status_code, 302)
+
+    def test_ (self):
+        with self.client as client:
+    def test_ (self):
+        with self.client as client:
+    def test_ (self):
+        with self.client as client:
+    def test_ (self):
+        with self.client as client:
